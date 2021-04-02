40ea07753c9864498761f4c3029dd9ced7a35409
Onkar Shingate <oshingate@gmail.com>
2021-04-02 16:58:40 +0530

    JS DOM Final Project change

--- block-BJaafa/code/script.js
+++ block-BJaafa/code/script.js
@@ -1,60 +1,86 @@
-let content = document.querySelector(".content");
+let totalMoves = 10;
+let yourMove = 0;
+let compMove = 0;
+let moveNo = 1;
 
-console.log(mainData);
-let data = [...mainData];
+let youdiv = document.querySelector(".you-div");
+let compdiv = document.querySelector(".comp-div");
+let clickHere = document.querySelector(".dice");
+let dicehead = document.querySelector(".dice-head");
+let dicebox = document.querySelector(".dice-box");
+let winnerName = document.querySelector(".winner-name");
+let winner = document.querySelector(".winner");
 
-// UI code
-let displayUI = (ele) => {
-  content.innerHTML = "";
-  let randomNo = Math.floor(Math.random() * data.length);
-
-  let method = data[randomNo];
-  console.log(method);
-
-  let namediv = document.createElement("div");
-  namediv.classList.add("div-name");
-  let nameh3 = document.createElement("h3");
-  nameh3.classList.add("name");
-  nameh3.innerText = method.name;
-  namediv.append(nameh3);
-
-  let paradiv = document.createElement("div");
-  paradiv.classList.add("div-para");
-  let parahead = document.createElement("h4");
-  parahead.classList.add("para-head");
-  parahead.innerText = "Parameters:";
-  let para = document.createElement("p");
-  para.innerText = method.parameter;
-  paradiv.append(parahead, para);
-
-  let returndiv = document.createElement("div");
-  returndiv.classList.add("div-return");
-  let returnhead = document.createElement("h4");
-  returnhead.classList.add("return-head");
-  returnhead.innerText = "Returns:";
-  let returns = document.createElement("p");
-  returns.innerText = method.return;
-  returndiv.append(returnhead, returns);
-
-  let examplediv = document.createElement("div");
-  examplediv.classList.add("div-example");
-  let examplehead = document.createElement("h4");
-  examplehead.classList.add("example-head");
-  examplehead.innerText = "Example:";
-  let example = document.createElement("p");
-  example.innerText = method.example;
-  examplediv.append(examplehead, example);
-
-  let aboutdiv = document.createElement("div");
-  aboutdiv.classList.add("div-about");
-  let abouthead = document.createElement("h4");
-  abouthead.classList.add("about-head");
-  abouthead.innerText = "about:";
-  let about = document.createElement("p");
-  about.innerText = method.about;
-  aboutdiv.append(abouthead, about);
-
-  content.append(namediv, paradiv, returndiv, examplediv, aboutdiv);
+// clickHereEvent
+let stopGame = () => {
+  clickHere.addEventListener("click", (event) => {
+    dicebox.innerHTML = "";
+  });
 };
+
+let clickHereEvent = (e) => {
+  moveNo++;
+  let randomNo = Math.floor(Math.random() * 6);
+  clickHere.innerHTML = `<img src=${dice[randomNo].photo} alt=""  />`;
+
+  if (moveNo % 2 === 0) {
+    if (yourMove + (randomNo + 1) < 10) {
+      yourMove += randomNo + 1;
+      console.log(yourMove);
+
+      if (yourMove == 9) {
+        winnerName.innerText = "You";
+        winner.classList.remove("winner-inactive");
+        stopGame();
+      }
+    }
+  } else {
+    if (compMove + (randomNo + 1) < 10) {
+      compMove += randomNo + 1;
+      if (compMove == 9) {
+        winnerName.innerText = "computer";
+        winner.classList.remove("winner-inactive");
+        stopGame();
+      }
+    }
+  }
+  displayUI();
+};
+// display ui func
+let displayUI = () => {
+  youdiv.innerHTML = "";
+  compdiv.innerHTML = "";
+  for (let i = 0; i < totalMoves; i++) {
+    let box1 = document.createElement("div");
+    box1.classList.add("box1");
+    box1.setAttribute("data-index", i);
+    youdiv.append(box1);
+
+    let box2 = document.createElement("div");
+    box2.classList.add("box2");
+    box2.setAttribute("data-index", i);
+    compdiv.append(box2);
+
+    if (i == yourMove && i == compMove) {
+      box1.innerHTML = `<img src="./assets/blue.jpeg" alt=""  />`;
+      box2.innerHTML = `<img src="./assets/red.png" alt=""  />`;
+    } else if (i == yourMove) {
+      box1.innerHTML = `<img src="./assets/blue.jpeg" alt=""  />`;
+    } else if (i == compMove) {
+      box2.innerHTML = `<img src="./assets/red.png" alt=""  />`;
+    } else {
+    }
+    if (moveNo % 2 === 1) {
+      dicehead.innerText = "Your Move";
+    } else {
+      dicehead.innerText = "Comp Move";
+    }
+  }
+  console.log(moveNo);
+};
+
+clickHere.addEventListener("click", (event) => {
+  clickHereEvent(event, moveNo);
+});
 
 displayUI();
